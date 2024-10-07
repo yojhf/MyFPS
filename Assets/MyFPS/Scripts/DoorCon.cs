0867fbc5a9e3250aa1743f5252224092e56419d5
DESKTOP-52351T8\Admin <yojhf@naver.com>
2024-10-07 17:22:06 +0900

    1. Raycast

--- Assets/MyFPS/Scripts/DoorCon.cs
+++ Assets/MyFPS/Scripts/DoorCon.cs
@@ -1,33 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace MyFPS
 {
     public class DoorCon : MonoBehaviour
     {
-        private bool isOpen = false;
+        [SerializeField] private GameObject actionUI;
+        [SerializeField] private string actionText = "Open the Door";
+        [SerializeField] private Transform soundObject;
+        public bool isOpen = false;
         Animator animator;
 
         // Start is called before the first frame update
         void Start()
         {
             animator = GetComponent<Animator>();
-
-            animator.SetBool("isOpen", true);
+            
         }
 
         // Update is called once per frame
         void Update()
         {
+            //DoorOpen();
+        }
 
+        private void OnMouseOver()
+        {
+            if(RayCon.length <= 2f)
+            {
+                actionUI.SetActive(true);
+                actionUI.transform.GetChild(1).GetComponent<TMP_Text>().text = actionText;
+
+                if(Input.GetButtonDown("Action"))
+                {
+                    animator.SetBool("isOpen", true);
+                    transform.GetComponent<Collider>().enabled = false;
+                    soundObject.GetComponent<AudioSource>().Play();
+                    actionUI.SetActive(false);
+                }
+            }
+        }
+        private void OnMouseExit()
+        {
+            actionUI.SetActive(false);
         }
 
         void DoorOpen()
         {
             if(isOpen == true)
             {
-              
+                actionUI.SetActive(true);
+                actionUI.transform.GetChild(1).GetComponent<TMP_Text>().text = actionText;
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    animator.SetBool("isOpen", true);
+                    transform.GetComponent<Collider>().enabled = false;
+                    isOpen = false;
+                }
+            }
+            else
+            {
+                actionUI.SetActive(false);
+                
             }
         }
 
